c234c8b58d4b48c959842204ec86423f47a86ef8
vivimortensen <vivi4531@stud.kea.dk>
2021-02-16 14:04:35 +0100

    1602 randomcolor files added

---
+++ randomcolor.js
@@ -0,0 +1,18 @@
+"use strict"; 
+
+//Random Color - Designing with functions exercise
+
+//sets the background to a random color
+randomBackground(){
+
+}
+
+//returns a random rgb color object
+randomColor(){
+
+}
+
+//converts a rgb color object into a CSS color string
+rgbToCSS( rgb ){
+
+}
